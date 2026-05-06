754301d7c67879d5438855cd610d4e5055387c16
deepj2013 <deepj2013@gmail.com>
2026-05-06 18:14:56 +0530

    change image

--- Client/src/pages/PublicHomePage.jsx
+++ Client/src/pages/PublicHomePage.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import { api } from "../api";
-import pvcPipeImage from "../assets/WhatsApp Image 2026-05-06 at 3.04.03 PM.jpeg";
+import pvcPipeImage from "../assets/WhatsApp Image 2026-05-06 at 6.13.04 PM.jpeg";
 import sprinklerPipeImage from "../assets/irrigation-sprinklers-500x500.webp";
 import miniSprinklerImage from "../assets/1718112762-13-dec-23.jpg";
 import onlinePanjikaranImage from "../assets/2422Farmer-Registration-System-can-empower-the-agri-ecosystem2 (1).jpg";
